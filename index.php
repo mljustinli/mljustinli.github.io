43c5c0f2605022790dfd69a584491c21983446ff
Justin Li <ml.justin.li@gmail.com>
2016-10-07 20:18:28 -0400

    Add initial website and frameworkish stuff (stylesheets, images)

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<!-- Custom -->
+	<link type="text/css" rel="stylesheet" href="css/stylesheet.css">
+	<link type="text/css" rel="stylesheet" media="screen and (min-width: 1201px)"` href="css/stylesheet-large.css" />
+	<link type="text/css" rel="stylesheet" media="screen and (min-width: 768px) and (max-width: 1200px)" href="css/stylesheet-small.css" />
+	<link type="text/css" rel="stylesheet" media="screen and (max-width: 767px)" href="css/stylesheet-mobile.css" />
+	<link type="text/css" rel="stylesheet" media="handheld" href="css/stylesheet-mobile.css" />
+
+	<link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
+	<link rel="icon" href="img/favicon.ico" type="image/x-icon">
+
+	<title>Juicy Tin</title>
+</head>
+<body>
+	<div id = "header">
+		
+	</div>
+	<div id = "header-label">
+		<span id = "header-span">Juic<span class = "remove-end-space">e</span><br/>Ti<span class = "remove-end-space">n</span></span>
+	</div>
+	<div class = "content" id = "image-content">
+		<img id = "big-logo" src = "img/HDLogo.jpg"/>
+	</div>
+	<!-- <div class = "content-divider"></div> -->
+	<div class = "content" id = "about-content">
+		<div class = "title"> About </div>
+	</div>
+	<!-- <div class = "content-divider"></div> -->
+	<div class = "content" id = "skills-content">
+		<div class = "title"> Skills </div>
+	</div>
+	
+</body>
+</html>
