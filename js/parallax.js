56963ffa4f6e456302655638bba477fa634eb1bc
Justin Li <ml.justin.li@gmail.com>
2016-10-10 09:03:20 -0400

    Remove parallax (ew) and darken background for about section

--- js/parallax.js
+++ js/parallax.js
@@ -1,9 +1,9 @@
-$(document).ready(function() {
-	$(document).scroll(function() {
-		var offset = $(document).scrollTop();
-		var height = $(document).height()/4;
-		var n = (offset * 0.5 + height).toString();
-		// alert(offset.top);
-		$("#about-content").css("background-position", "center " + n + "px");
-	});
-});
+// $(document).ready(function() {
+// 	$(document).scroll(function() {
+// 		var offset = $(document).scrollTop();
+// 		var height = $(document).height()/4;
+// 		var n = (offset * 0.5 + height).toString();
+// 		// alert(offset.top);
+// 		$("#about-content").css("background-position", "center " + n + "px");
+// 	});
+// });
