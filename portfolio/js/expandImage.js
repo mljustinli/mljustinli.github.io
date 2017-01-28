c84105ad1ac533e3e256cba14f3021a35b4ddc80
Justin Li <ml.justin.li@gmail.com>
2017-01-28 17:40:49 -0500

    Remove caption for now.

--- portfolio/js/expandImage.js
+++ portfolio/js/expandImage.js
@@ -1,23 +1,3 @@
-// var modal = document.getElementById('myModal');
-
-// // Get the image and insert it inside the modal - use its "alt" text as a caption
-// var img = document.getElementById('myImg');
-// var modalImg = document.getElementById("img01");
-// var captionText = document.getElementById("caption");
-// img.onclick = function(){
-//     modal.style.display = "block";
-//     modalImg.src = this.src;
-//     captionText.innerHTML = this.alt;
-// }
-
-// // Get the <span> element that closes the modal
-// var span = document.getElementsByClassName("close")[0];
-
-// // When the user clicks on <span> (x), close the modal
-// span.onclick = function() { 
-//   modal.style.display = "none";
-// }
-
 /*
 	Many thanks to w3schools
 */
@@ -32,7 +12,11 @@ $(".portfolio-image").on("click", function(e) {
 	var captionText = document.getElementById("caption");
 	modal.style.display = "block";
 	modalImg.src = this.src;
-	captionText.innerHTML = "hi there";
+
+	/*Change this if you want caption text*/
+	captionText.innerHTML = "";
+	/* Or use this line for the alt-text*/
+	// captionText.innerHTML = this.alt;
 
 	// Get the <span> element that closes the modal
 	var span = document.getElementsByClassName("close")[0];
