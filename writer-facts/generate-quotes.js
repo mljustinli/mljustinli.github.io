990eda71809fe3ba55b1a03c7aeef5e1d1fcc0c4
mljustinli <ml.justin.li@gmail.com>
2018-09-28 17:24:17 -0400

    Adjust size of text so all cards fit

--- writer-facts/generate-quotes.js
+++ writer-facts/generate-quotes.js
@@ -14,17 +14,16 @@ var kazuo = {
 var coleridge = {
     name: "Samuel Taylor Coleridge",
     fact: "Samuel Coleridge was an American poet most well known for <em>The Rime of the Ancient Mariner</em> and other poems. "
-        + "\"Most of the poems for which he is remembered were written when he was in his mid-twenties."
-        + " After that, any ambitious writing project inspired in him what he called 'an indefinite indescribable Terror,'"
+        + "After his mid-twenties, \"any ambitious writing project inspired in him what he called 'an indefinite indescribable Terror,'"
         + " and he wasted much of the rest of his life on opium addiction.\"",
-    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/SamuelTaylorColeridge.jpg/1200px-SamuelTaylorColeridge.jpg",
+    img: "https://www.bl.uk/britishlibrary/~/media/bl/global/dl%20romantics%20and%20victorians/portraits/dl-portrait-npg-samuel-taylor-coleridge.jpg?crop=1&cropX=0&cropY=136&cropW=1610&cropH=905&w=800&h=450&dispW=800&dispH=450",
     src: "https://www.theatlantic.com/entertainment/archive/2017/10/writing-advice-from-a-newly-minted-nobel-winner/542136/"
 };
 
 var hammett = {
     name: "Dashiell Hammett",
     fact: "American author, Dashiell Hammett, is famous for his detective novels such as <em>The Maltese Falcon</em> and <em>The Thin Man</em>. "
-        + "\"In 1934, when he was thirty-nine, his fifth novel, \"The Thin Man,\" came out, and that was the end, though he lived for almost three more decades. In his later life, he said that he stopped publishing because he felt he was repeating himself: 'It is the beginning of the end when you discover that you have a style'...He wrote and wrote, but he never accomplished anything that satisfied him.\"",
+        + "\"In his later life, he said that he stopped publishing because he felt he was repeating himself: 'It is the beginning of the end when you discover that you have a style'...He wrote and wrote, but he never accomplished anything that satisfied him.\"",
     img: "https://www.theparisreview.org/blog/wp-content/uploads/2018/09/maxresdefault-2.jpg",
     src: "https://www.newyorker.com/magazine/2004/06/14/blocked"
 };
@@ -39,21 +38,21 @@ var lee = {
 var ellison = {
     name: "Ralph Ellison",
     fact: "Ralph Ellison is well known for his novel, <em>The Invisible Man</em>. After, his second novel \"grew and grew. Eventually, he thought it might require three volumes. He worked on it for forty years, until he died in 1994, at the age of eighty, leaving behind more than two thousand pages of manuscript and notes.\"",
-    img: "https://www.biography.com/.image/t_share/MTE5NDg0MDU0OTYwMzA5Nzc1/ralph-ellison-9286702-1-402.jpg",
+    img: "https://aalbc.com/author-photos/ralph-ellison.jpg",
     src: "https://www.newyorker.com/magazine/2004/06/14/blocked"
 };
 
 var gaiman = {
     name: "Neil Gaiman",
     fact: "Neil Gaiman is known for <em>Coraline</em>, <em>Neverwhere</em>, and <em>American Gods</em>, among other novels. Gaiman says, \"I don't really believe in writer's block, but I absolutely believe in getting stuck.\" Gaiman's solution is to always keep a separate project on the back burner so that he can take a break from the material that's troubling him.",
-    img: "https://upload.wikimedia.org/wikipedia/commons/b/bc/Kyle-cassidy-neil-gaiman-April-2013.jpg",
+    img: "https://pmcvariety.files.wordpress.com/2017/06/neil-gaiman-credit-sasha-maslav_the-new-york-times_redux.jpg?w=1000",
     src: "https://www.huffingtonpost.com/entry/neil-gaiman-doesnt-believe-in-writers-block_us_56421d28e4b0b24aee4be334"
 };
 
 var woolf = {
     name: "Virginia Woolf",
     fact: "Virginia Woolf is known for her stream-of-consciousness storytelling, which comes through in her novel <em>Mrs. Dalloway</em>. She \"battled with bouts of writer's block throughout her life due to deep depressions brought on by suspected bipolar disorder.\"",
-    img: "https://upload.wikimedia.org/wikipedia/commons/0/0b/George_Charles_Beresford_-_Virginia_Woolf_in_1902_-_Restoration.jpg",
+    img: "https://cms.qz.com/wp-content/uploads/2018/01/george_charles_beresford_-_virginia_woolf_in_1902-e1516902098788.jpg?quality=75&strip=all&w=410&h=231",
     src: "https://indiereader.com/2012/09/7-pop-culture-icons-who-famously-suffered-from-writers-block/"
 };
 
